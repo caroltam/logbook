f71a810a7eae4ef204fee4466dc306ac6be95587
caroltam <caroltam@users.noreply.github.com>
2023-09-15 09:29:30 +0000

    deployed via Gatsby Publish Action 🎩 for a3940e2de5fd31d90f24c7178bacd86da06e6ef8

---
+++ component---src-pages-index-js-184f6595126a92d17456.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklogbook=self.webpackChunklogbook||[]).push([[678],{6558:function(e,t,l){l.r(t),l.d(t,{Head:function(){return a}});var n=l(7294);const a=()=>n.createElement("title",null,"Home Page");t.default=()=>n.createElement("main",null,n.createElement("h1",null,"Welcome to my Gatsby site!"),n.createElement("p",null,"I'm making this by following the Gatsby Tutorial."))}}]);
+//# sourceMappingURL=component---src-pages-index-js-184f6595126a92d17456.js.map
